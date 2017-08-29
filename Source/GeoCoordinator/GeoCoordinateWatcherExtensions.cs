19c190f67509cff2dca4af7a58405a30e647068a
Atsushi Nakamura <nuits.jp@live.jp>
2017-08-29 23:58:38 +0900

    Add Project.

---
+++ Source/GeoCoordinator/GeoCoordinateWatcherExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeoCoordinator
+{
+    public static class GeoCoordinateWatcherExtensions
+    {
+        public static GeoCoordinate GetCurrentLocation(this GeoCoordinateWatcher watcher, TimeSpan timeout)
+        {
+            // PositionChangedイベントを監視し、イベント発生時に待機中のスレッドを再開する
+            watcher.PositionChanged += (sender, eventArgs) =>
+            {
+                Monitor.Enter(watcher);
+                try
+                {
+                    Monitor.PulseAll(watcher);
+                }
+                finally
+                {
+                    Monitor.Exit(watcher);
+                }
+            };
+
+            Monitor.Enter(watcher);
+            try
+            {
+                // GeoCoordinateWatcherを起動し、PositionChangedイベントが発生するか
+                // タイムアウトまで待機する
+                watcher.Start();
+                Monitor.Wait(watcher, timeout);
+            }
+            finally
+            {
+                Monitor.Exit(watcher);
+            }
+            watcher.Stop();
+            return watcher.Position.Location;
+        }
+    }
+}
